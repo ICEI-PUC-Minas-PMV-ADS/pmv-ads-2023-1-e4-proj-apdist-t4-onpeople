6eed1e90c68a27cd81b8dfd2b522c994339dfb5e
alexsgaldino <94413402+alexgaldinopuc@users.noreply.github.com>
2023-05-28 10:56:57 -0300

    Review field INPUT using mat-input and jobRole CRUD

--- src/OnPeople/BackEnd/src/OnPeople.Integration/Models/Dashboard/DashboardDepartamento.cs
+++
@@ -1,9 +0,0 @@
-using OnPeople.Integration.Models.Links;
-
-namespace OnPeople.Integration.Models.Dashboard
-{
-    public class DashboardDepartamento : LinksHATEOS
-    {
-        public int CountDepartamentos { get; set; }
-    }
-}
